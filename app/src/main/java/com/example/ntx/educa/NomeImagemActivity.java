0f72f6d6ec965a1ddef5a15e5b51dc9f9868f89c
JoaoNeto <by.srneto@gmail.com>
2018-06-12 13:56:02 -0300

    Inicio do desenvolvimento do novo jogo:
    * Nome da imagem

---
+++ app/src/main/java/com/example/ntx/educa/NomeImagemActivity.java
@@ -0,0 +1,13 @@
+package com.example.ntx.educa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NomeImagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nome_imagem);
+    }
+}
